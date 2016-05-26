e51d58d388213707a55fe89e4741c4bfb3162954
rix1 <rikardeide@gmail.com>
2016-05-26 17:11:24 +0200

    updaed filename and removed unussed code

---
+++ time-synchronizer.js
@@ -0,0 +1,25 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+var PORT = 8081;
+
+app.listen(PORT);
+console.log('Server listening at http://localhost:' + PORT);
+
+function handler (req, res) {
+	console.log('request', req.url);
+	res.writeHead(404);
+	res.end('Not found');
+}
+
+io.on('connection', function (socket) {
+	console.log('socket connection established');
+	socket.on('timesync', function (data) {
+		console.log('message', data);
+		socket.emit('timesync', {
+			id: data && 'id' in data ? data.id : null,
+			result: Date.now()
+		});
+	});
+});
